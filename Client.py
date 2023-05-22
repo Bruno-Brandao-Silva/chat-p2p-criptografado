14b2fdda3f5219207a735080391bb667dbb75de6
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-05-22 14:21:13 -0300

    Criptográfia

--- Client.py
+++ Client.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python
 
 import socket
-import sys
 import time
 import threading
 import select
-import traceback
+import json
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+simetric_key = None
+private_key = None
+public_key_extern = None
+fernet = None
+public_key_bytes = None
+send_public_key = True
 
 
 class Server(threading.Thread):
@@ -13,18 +22,100 @@ class Server(threading.Thread):
         self.receive = receive
 
     def run(self):
+        global simetric_key
+        global private_key
+        global public_key_extern
+        global fernet
+        global send_public_key
+        global public_key_bytes
         lis = []
         lis.append(self.receive)
-        while 1:
+        while True:
             read, write, err = select.select(lis, [], [])
             for item in read:
                 try:
-                    s = item.recv(1024)
-                    if s != '':
-                        chunk = s
-                        print(chunk.decode() + '\n>>')
+                    chunk = item.recv(2048)
+                    if chunk != '':
+                        try:
+                            data = json.loads(chunk.decode())
+                            if data['tag'] == 'PK':
+                                send_public_key = False
+                                bytes_PKE = data['content'].encode()
+                                public_key_extern = serialization.load_pem_public_key(
+                                    bytes_PKE)
+                                if simetric_key is None:
+                                    simetric_key = Fernet.generate_key()
+                                    fernet = Fernet(simetric_key)
+                                    print("P2P Criptografado")
+                                simetric_key_encrypted = public_key_extern.encrypt(
+                                    simetric_key,
+                                    padding.OAEP(
+                                        mgf=padding.MGF1(
+                                            algorithm=hashes.SHA256()),
+                                        algorithm=hashes.SHA256(),
+                                        label=None
+                                    )
+                                )
+                                encrypted_hash = private_key.sign(
+                                    simetric_key,
+                                    padding.PSS(
+                                        mgf=padding.MGF1(hashes.SHA256()),
+                                        salt_length=padding.PSS.MAX_LENGTH
+                                    ),
+                                    hashes.SHA256()
+                                )
+                                json_data = json.dumps(
+                                    {
+                                        'tag': 'SK',
+                                        'simetric_key_encrypted': simetric_key_encrypted.hex(),
+                                        'public_key_bytes': public_key_bytes.decode(),
+                                        'encrypted_hash': encrypted_hash.hex()
+                                    })
+                                self.receive.sendall(json_data.encode())
+
+                            elif data['tag'] == 'SK' and simetric_key is None:
+                                try:
+                                    bytes_encrypted = bytes.fromhex(
+                                        data['simetric_key_encrypted'])
+                                    simetric_key = private_key.decrypt(
+                                        bytes_encrypted,
+                                        padding.OAEP(
+                                            mgf=padding.MGF1(
+                                                algorithm=hashes.SHA256()),
+                                            algorithm=hashes.SHA256(),
+                                            label=None
+                                        )
+                                    )
+                                    bytes_PKE = data['public_key_bytes'].encode(
+                                    )
+                                    public_key_extern = serialization.load_pem_public_key(
+                                        bytes_PKE)
+                                    bytes_encrypted = bytes.fromhex(
+                                        data['encrypted_hash'])
+                                    public_key_extern.verify(
+                                        bytes_encrypted,
+                                        simetric_key,
+                                        padding.PSS(
+                                            mgf=padding.MGF1(
+                                                hashes.SHA256()),
+                                            salt_length=padding.PSS.MAX_LENGTH
+                                        ),
+                                        hashes.SHA256()
+                                    )
+                                    fernet = Fernet(simetric_key)
+                                    print("P2P Criptografado")
+                                except:
+                                    simetric_key = None
+
+                            elif data['tag'] == 'encrypted' and fernet is not None:
+                                msg = fernet.decrypt(data['content'])
+                                msg = msg.decode()
+                                print(msg + '\n>>')
+                        except Exception as e:
+                            print(e)
                 except:
-                    traceback.print_exc(file=sys.stdout)
+                    print("lost connection")
+                    time.sleep(1)
                     break
 
 
@@ -32,11 +123,14 @@ class Client(threading.Thread):
     def connect(self, host, port):
         self.sock.connect((host, port))
 
-    def client(self, host, port, msg):
-        sent = self.sock.send(msg)
-        # print "Sent\n"
+    def client(self, msg):
+        self.sock.send(msg)
 
     def run(self):
+        global simetric_key
+        global private_key
+        global fernet
+        global public_key_bytes
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         try:
@@ -47,28 +141,48 @@ class Client(threading.Thread):
             return 1
 
         print("Connecting\n")
-        s = ''
         self.connect(host, port)
         print("Connected\n")
         user_name = input("Enter the User Name to be Used\n>>")
-        receive = self.sock
         time.sleep(1)
         srv = Server()
-        srv.initialise(receive)
+        srv.initialise(self.sock)
         srv.daemon = True
+
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        public_key = private_key.public_key()
+        public_key_bytes = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        json_data = json.dumps(
+            {'tag': 'PK', 'content': public_key_bytes.decode()})
+
         print("Starting service")
         srv.start()
-        while 1:
-            # print "Waiting for message\n"
-            msg = input('>>')
-            if msg == 'exit':
-                break
-            if msg == '':
-                continue
-            # print "Sending\n"
-            msg = user_name + ': ' + msg
-            data = msg.encode()
-            self.client(host, port, data)
+        time.sleep(1)
+
+        if send_public_key:
+            self.client(json_data.encode())
+
+        while True:
+            try:
+                if fernet is not None:
+                    msg = input('>>')
+                    if msg == 'exit':
+                        break
+                    if msg == '':
+                        continue
+                    msg = user_name + ': ' + msg
+                    msg = fernet.encrypt(msg.encode())
+                    data = json.dumps(
+                        {'tag': 'encrypted', 'content': msg.decode()})
+                    self.client(data.encode())
+            except Exception as e:
+                print(e)
         return (1)
 
 
--- Server.py
+++ Server.py
@@ -1,8 +1,4 @@
-#! /usr/bin/env python
-
 import socket
-import sys
-import traceback
 import threading
 import select
 
@@ -12,63 +8,59 @@ SENT_BY = {}
 
 
 class Server(threading.Thread):
-
-    def init(self):
+    def __init__(self):
+        super().__init__()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        self.sock.bind(('', 5535))
+        self.sock.bind(('localhost', 5535))
         self.sock.listen(2)
         SOCKET_LIST.append(self.sock)
         print("Server started on port 5535")
 
     def run(self):
-        while 1:
-            read, write, err = select.select(SOCKET_LIST, [], [], 0)
-            for sock in read:
-                if sock == self.sock:
-                    sockfd, addr = self.sock.accept()
-                    print(str(addr))
-
-                    SOCKET_LIST.append(sockfd)
-                    print(SOCKET_LIST[len(SOCKET_LIST) - 1])
-
-                else:
-                    try:
-                        s = sock.recv(1024)
-                        if s == '':
-                            print(str(sock.getpeername()))
-                            continue
-                        else:
-                            TO_BE_SENT.append(s)
-                            SENT_BY[s] = (str(sock.getpeername()))
-                    except:
-                        print(str(sock.getpeername()))
+        while True:
+            try:
+                read, _, err = select.select(SOCKET_LIST, [], [], 0)
+                for sock in read:
+                    if sock == self.sock:
+                        sockfd, addr = self.sock.accept()
+                        print(f"New client connected from {addr}")
+                        SOCKET_LIST.append(sockfd)
+                    else:
+                        s = sock.recv(2048)
+                        TO_BE_SENT.append(s)
+                        SENT_BY[s] = sock
+            except Exception as e:
+                print(f"Client {sock.getpeername()} disconnected")
+                SOCKET_LIST.remove(sock)
 
 
-class handle_connections(threading.Thread):
+class HandleConnections(threading.Thread):
     def run(self):
-        while 1:
-            read, write, err = select.select([], SOCKET_LIST, [], 0)
-            for items in TO_BE_SENT:
-                for s in write:
-                    try:
-                        if (str(s.getpeername()) == SENT_BY[items]):
-                            print("Ignoring %s" % (str(s.getpeername())))
-                            continue
-                        print("Sending to %s" % (str(s.getpeername())))
-                        s.send(items)
-
-                    except:
-                        traceback.print_exc(file=sys.stdout)
-                TO_BE_SENT.remove(items)
-                del (SENT_BY[items])
+        while True:
+            try:
+                _, write, _ = select.select([], SOCKET_LIST, [], 0)
+                for data in list(TO_BE_SENT):
+                    for sock in write:
+                        if sock != SENT_BY.get(data):
+                            print(
+                                f"Sending {len(data)} bytes to {sock.getpeername()}")
+                            try:
+                                sock.send(data)
+                            except Exception as e:
+                                print(
+                                    f"Error occurred while sending data to {sock.getpeername()}: {e}")
+                                continue
+                    TO_BE_SENT.remove(data)
+                    del SENT_BY[data]
+            except Exception as e:
+                print(f"Error occurred while writing to socket: {e}")
 
 
 if __name__ == '__main__':
-    srv = Server()
-    srv.init()
-    srv.start()
-    print(SOCKET_LIST)
-    handle = handle_connections()
-    handle.start()
+    server = Server()
+    server.start()
+
+    handler = HandleConnections()
+    handler.start()
